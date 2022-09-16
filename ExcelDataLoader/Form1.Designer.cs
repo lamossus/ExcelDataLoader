d00f7fe819b89c98227d18ca146155aa079232c6
Chetverkin Mikhail <lamossus@gmail.com>
2022-09-16 16:28:56 +0300

    Update

--- ExcelDataLoader/Form1.Designer.cs
+++ ExcelDataLoader/Form1.Designer.cs
@@ -28,10 +28,30 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
 			this.load_excel_button = new System.Windows.Forms.Button();
-			this.excel_file_name = new System.Windows.Forms.Label();
 			this.table_combo_box = new System.Windows.Forms.ComboBox();
 			this.upload_button = new System.Windows.Forms.Button();
+			this.excel_preview = new System.Windows.Forms.DataGridView();
+			this.map_column1 = new System.Windows.Forms.ComboBox();
+			this.map_column2 = new System.Windows.Forms.ComboBox();
+			this.map_column3 = new System.Windows.Forms.ComboBox();
+			this.map_column4 = new System.Windows.Forms.ComboBox();
+			this.excel_filename = new System.Windows.Forms.TextBox();
+			this.protocol_text = new System.Windows.Forms.TextBox();
+			this.db_table_label = new System.Windows.Forms.Label();
+			this.clear_table_chechBox = new System.Windows.Forms.CheckBox();
+			this.skip_rows_numeric = new System.Windows.Forms.NumericUpDown();
+			this.skip_rows_label = new System.Windows.Forms.Label();
+			this.login_label = new System.Windows.Forms.Label();
+			this.login_textBox = new System.Windows.Forms.TextBox();
+			this.password_label = new System.Windows.Forms.Label();
+			this.password_textBox = new System.Windows.Forms.TextBox();
+			this.table_label = new System.Windows.Forms.Label();
+			this.table_name_textBox = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.excel_preview)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.skip_rows_numeric)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// load_excel_button
@@ -44,13 +64,6 @@
 			this.load_excel_button.UseVisualStyleBackColor = true;
 			this.load_excel_button.Click += new System.EventHandler(this.load_excel_button_Click);
 			// 
-			// excel_file_name
-			// 
-			this.excel_file_name.Location = new System.Drawing.Point(115, 16);
-			this.excel_file_name.Name = "excel_file_name";
-			this.excel_file_name.Size = new System.Drawing.Size(314, 15);
-			this.excel_file_name.TabIndex = 1;
-			// 
 			// table_combo_box
 			// 
 			this.table_combo_box.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
@@ -58,41 +71,279 @@
 			this.table_combo_box.Items.AddRange(new object[] {
             "fond_debts",
             "fond_not_boo"});
-			this.table_combo_box.Location = new System.Drawing.Point(12, 41);
+			this.table_combo_box.Location = new System.Drawing.Point(115, 41);
 			this.table_combo_box.Name = "table_combo_box";
-			this.table_combo_box.Size = new System.Drawing.Size(417, 23);
+			this.table_combo_box.Size = new System.Drawing.Size(179, 23);
 			this.table_combo_box.TabIndex = 2;
 			// 
 			// upload_button
 			// 
-			this.upload_button.Location = new System.Drawing.Point(359, 272);
+			this.upload_button.Location = new System.Drawing.Point(234, 311);
 			this.upload_button.Name = "upload_button";
-			this.upload_button.Size = new System.Drawing.Size(70, 68);
+			this.upload_button.Size = new System.Drawing.Size(216, 25);
 			this.upload_button.TabIndex = 3;
 			this.upload_button.Text = "Загрузить данные";
 			this.upload_button.UseVisualStyleBackColor = true;
 			this.upload_button.Click += new System.EventHandler(this.upload_button_Click);
 			// 
+			// excel_preview
+			// 
+			this.excel_preview.AllowUserToAddRows = false;
+			this.excel_preview.AllowUserToDeleteRows = false;
+			this.excel_preview.AllowUserToResizeColumns = false;
+			this.excel_preview.AllowUserToResizeRows = false;
+			this.excel_preview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.excel_preview.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.excel_preview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+			this.excel_preview.ColumnHeadersVisible = false;
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.excel_preview.DefaultCellStyle = dataGridViewCellStyle2;
+			this.excel_preview.Location = new System.Drawing.Point(12, 99);
+			this.excel_preview.MultiSelect = false;
+			this.excel_preview.Name = "excel_preview";
+			this.excel_preview.ReadOnly = true;
+			this.excel_preview.RowHeadersVisible = false;
+			this.excel_preview.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+			this.excel_preview.RowTemplate.Height = 25;
+			this.excel_preview.ScrollBars = System.Windows.Forms.ScrollBars.None;
+			this.excel_preview.Size = new System.Drawing.Size(438, 101);
+			this.excel_preview.TabIndex = 5;
+			// 
+			// map_column1
+			// 
+			this.map_column1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.map_column1.FormattingEnabled = true;
+			this.map_column1.Items.AddRange(new object[] {
+            "id",
+            "ogrn",
+            "inn",
+            "name"});
+			this.map_column1.Location = new System.Drawing.Point(12, 70);
+			this.map_column1.Name = "map_column1";
+			this.map_column1.Size = new System.Drawing.Size(105, 23);
+			this.map_column1.TabIndex = 6;
+			this.map_column1.Tag = "0";
+			this.map_column1.SelectedIndexChanged += new System.EventHandler(this.MappingChanged);
+			// 
+			// map_column2
+			// 
+			this.map_column2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.map_column2.FormattingEnabled = true;
+			this.map_column2.Items.AddRange(new object[] {
+            "id",
+            "ogrn",
+            "inn",
+            "name"});
+			this.map_column2.Location = new System.Drawing.Point(123, 70);
+			this.map_column2.Name = "map_column2";
+			this.map_column2.Size = new System.Drawing.Size(105, 23);
+			this.map_column2.TabIndex = 7;
+			this.map_column2.Tag = "1";
+			this.map_column2.SelectedIndexChanged += new System.EventHandler(this.MappingChanged);
+			// 
+			// map_column3
+			// 
+			this.map_column3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.map_column3.FormattingEnabled = true;
+			this.map_column3.Items.AddRange(new object[] {
+            "id",
+            "ogrn",
+            "inn",
+            "name"});
+			this.map_column3.Location = new System.Drawing.Point(234, 70);
+			this.map_column3.Name = "map_column3";
+			this.map_column3.Size = new System.Drawing.Size(105, 23);
+			this.map_column3.TabIndex = 8;
+			this.map_column3.Tag = "2";
+			this.map_column3.SelectedIndexChanged += new System.EventHandler(this.MappingChanged);
+			// 
+			// map_column4
+			// 
+			this.map_column4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.map_column4.FormattingEnabled = true;
+			this.map_column4.Items.AddRange(new object[] {
+            "id",
+            "ogrn",
+            "inn",
+            "name"});
+			this.map_column4.Location = new System.Drawing.Point(345, 70);
+			this.map_column4.Name = "map_column4";
+			this.map_column4.Size = new System.Drawing.Size(105, 23);
+			this.map_column4.TabIndex = 9;
+			this.map_column4.Tag = "3";
+			this.map_column4.SelectedIndexChanged += new System.EventHandler(this.MappingChanged);
+			// 
+			// excel_filename
+			// 
+			this.excel_filename.Location = new System.Drawing.Point(115, 12);
+			this.excel_filename.Name = "excel_filename";
+			this.excel_filename.ReadOnly = true;
+			this.excel_filename.Size = new System.Drawing.Size(179, 23);
+			this.excel_filename.TabIndex = 10;
+			// 
+			// protocol_text
+			// 
+			this.protocol_text.Location = new System.Drawing.Point(12, 206);
+			this.protocol_text.Multiline = true;
+			this.protocol_text.Name = "protocol_text";
+			this.protocol_text.ReadOnly = true;
+			this.protocol_text.Size = new System.Drawing.Size(438, 70);
+			this.protocol_text.TabIndex = 11;
+			// 
+			// db_table_label
+			// 
+			this.db_table_label.AutoSize = true;
+			this.db_table_label.Location = new System.Drawing.Point(12, 44);
+			this.db_table_label.Name = "db_table_label";
+			this.db_table_label.Size = new System.Drawing.Size(75, 15);
+			this.db_table_label.TabIndex = 12;
+			this.db_table_label.Text = "Таблица БД:";
+			// 
+			// clear_table_chechBox
+			// 
+			this.clear_table_chechBox.AutoSize = true;
+			this.clear_table_chechBox.Checked = true;
+			this.clear_table_chechBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.clear_table_chechBox.Location = new System.Drawing.Point(300, 15);
+			this.clear_table_chechBox.Name = "clear_table_chechBox";
+			this.clear_table_chechBox.Size = new System.Drawing.Size(123, 19);
+			this.clear_table_chechBox.TabIndex = 13;
+			this.clear_table_chechBox.Text = "Очистка таблицы";
+			this.clear_table_chechBox.UseVisualStyleBackColor = true;
+			// 
+			// skip_rows_numeric
+			// 
+			this.skip_rows_numeric.Location = new System.Drawing.Point(415, 41);
+			this.skip_rows_numeric.Name = "skip_rows_numeric";
+			this.skip_rows_numeric.Size = new System.Drawing.Size(35, 23);
+			this.skip_rows_numeric.TabIndex = 14;
+			this.skip_rows_numeric.ValueChanged += new System.EventHandler(this.skip_rows_numeric_ValueChanged);
+			// 
+			// skip_rows_label
+			// 
+			this.skip_rows_label.AutoSize = true;
+			this.skip_rows_label.Location = new System.Drawing.Point(300, 44);
+			this.skip_rows_label.Name = "skip_rows_label";
+			this.skip_rows_label.Size = new System.Drawing.Size(109, 15);
+			this.skip_rows_label.TabIndex = 15;
+			this.skip_rows_label.Text = "Пропустить строк:";
+			// 
+			// login_label
+			// 
+			this.login_label.AutoSize = true;
+			this.login_label.Location = new System.Drawing.Point(12, 287);
+			this.login_label.Name = "login_label";
+			this.login_label.Size = new System.Drawing.Size(44, 15);
+			this.login_label.TabIndex = 16;
+			this.login_label.Text = "Логин:";
+			// 
+			// login_textBox
+			// 
+			this.login_textBox.Location = new System.Drawing.Point(75, 284);
+			this.login_textBox.Name = "login_textBox";
+			this.login_textBox.Size = new System.Drawing.Size(153, 23);
+			this.login_textBox.TabIndex = 17;
+			// 
+			// password_label
+			// 
+			this.password_label.AutoSize = true;
+			this.password_label.Location = new System.Drawing.Point(12, 316);
+			this.password_label.Name = "password_label";
+			this.password_label.Size = new System.Drawing.Size(52, 15);
+			this.password_label.TabIndex = 18;
+			this.password_label.Text = "Пароль:";
+			// 
+			// password_textBox
+			// 
+			this.password_textBox.Location = new System.Drawing.Point(75, 313);
+			this.password_textBox.Name = "password_textBox";
+			this.password_textBox.Size = new System.Drawing.Size(153, 23);
+			this.password_textBox.TabIndex = 19;
+			// 
+			// table_label
+			// 
+			this.table_label.AutoSize = true;
+			this.table_label.Location = new System.Drawing.Point(234, 287);
+			this.table_label.Name = "table_label";
+			this.table_label.Size = new System.Drawing.Size(57, 15);
+			this.table_label.TabIndex = 20;
+			this.table_label.Text = "Таблица:";
+			// 
+			// table_name_textBox
+			// 
+			this.table_name_textBox.Location = new System.Drawing.Point(297, 282);
+			this.table_name_textBox.Name = "table_name_textBox";
+			this.table_name_textBox.Size = new System.Drawing.Size(153, 23);
+			this.table_name_textBox.TabIndex = 21;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(441, 352);
+			this.ClientSize = new System.Drawing.Size(462, 385);
+			this.Controls.Add(this.table_name_textBox);
+			this.Controls.Add(this.table_label);
+			this.Controls.Add(this.password_textBox);
+			this.Controls.Add(this.password_label);
+			this.Controls.Add(this.login_textBox);
+			this.Controls.Add(this.login_label);
+			this.Controls.Add(this.skip_rows_label);
+			this.Controls.Add(this.skip_rows_numeric);
+			this.Controls.Add(this.clear_table_chechBox);
+			this.Controls.Add(this.db_table_label);
+			this.Controls.Add(this.protocol_text);
+			this.Controls.Add(this.excel_filename);
+			this.Controls.Add(this.map_column4);
+			this.Controls.Add(this.map_column3);
+			this.Controls.Add(this.map_column2);
+			this.Controls.Add(this.map_column1);
+			this.Controls.Add(this.excel_preview);
 			this.Controls.Add(this.upload_button);
 			this.Controls.Add(this.table_combo_box);
-			this.Controls.Add(this.excel_file_name);
 			this.Controls.Add(this.load_excel_button);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.excel_preview)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.skip_rows_numeric)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private Button load_excel_button;
-		private Label excel_file_name;
 		private ComboBox table_combo_box;
 		private Button upload_button;
+		private DataGridView excel_preview;
+		private ComboBox map_column1;
+		private ComboBox map_column2;
+		private ComboBox map_column3;
+		private ComboBox map_column4;
+		private TextBox excel_filename;
+		private TextBox protocol_text;
+		private Label db_table_label;
+		private CheckBox clear_table_chechBox;
+		private NumericUpDown skip_rows_numeric;
+		private Label skip_rows_label;
+		private Label login_label;
+		private TextBox login_textBox;
+		private Label password_label;
+		private TextBox password_textBox;
+		private Label table_label;
+		private TextBox table_name_textBox;
 	}
 }
--- ExcelDataLoader/Form1.cs
+++ ExcelDataLoader/Form1.cs
@@ -15,11 +15,22 @@ namespace ExcelDataLoader
 
 		private string _excelPath = "";
 
+		private Dictionary<string, int> _mapping = new Dictionary<string, int>() { { "id", 0 }, { "ogrn", 1 }, { "inn", 2 }, { "name", 3 } };
+		private List<ComboBox> _mappingBoxes = new List<ComboBox>();
+
 		public Form1()
 		{
 			InitializeComponent();
 
 			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+			_mappingBoxes.Add(map_column1);
+			_mappingBoxes.Add(map_column2);
+			_mappingBoxes.Add(map_column3);
+			_mappingBoxes.Add(map_column4);
+
+			for (int i = 0; i < _mappingBoxes.Count; i++)
+				_mappingBoxes[i].SelectedIndex = i;
 		}
 
 		private void load_excel_button_Click(object sender, EventArgs e)
@@ -29,28 +40,38 @@ namespace ExcelDataLoader
 				openFileDialog.InitialDirectory = "C:\\";
 				openFileDialog.Filter = "Excel files|*.xlsx;*.xls";
 
-
 				if (openFileDialog.ShowDialog() == DialogResult.OK)
 				{
 					_excelPath = openFileDialog.FileName;
-					excel_file_name.Text = openFileDialog.SafeFileName;
+					excel_filename.Text = openFileDialog.SafeFileName;
 				}
+				else
+					return;
 			}
+
+			excel_preview.DataSource = GetExcelPreview(_excelPath, 4, 4, (int)skip_rows_numeric.Value);
 		}
 
 		private void upload_button_Click(object sender, EventArgs e)
 		{
-			Stopwatch stopwatch = new Stopwatch();
+			string tableName = table_combo_box.Text;
 
-			stopwatch.Start();
-			DataTable dt = GetExcelData(_excelPath);
-			Debug.Write(stopwatch.Elapsed.ToString() + '\n');
-			stopwatch.Restart();
-			BulkInsert(dt, table_combo_box.Text);
-			Debug.Write(stopwatch.Elapsed);
+			StringBuilder protocolStringBuiler = new StringBuilder("Протокол:\n");
+
+			protocolStringBuiler.AppendLine($"Таблица-преемник: {tableName}");
+			
+			using (var con = new MySqlConnection(CON_STRING))
+			{
+				protocolStringBuiler.AppendLine($"Количество записей до очистки: {GetRowCount(tableName, con)}");
+				protocolStringBuiler.AppendLine($"Имя загружаемого файла: {Path.GetFileName(_excelPath)}");
+				DataTable dt = GetExcelData(_excelPath, (int)skip_rows_numeric.Value);
+				BulkInsert(dt, table_combo_box.Text, con, clear_table_chechBox.Checked);
+				protocolStringBuiler.AppendLine($"Количество записей после загрузки: {GetRowCount(tableName, con)}");
+			}
+
+			protocol_text.Text = protocolStringBuiler.ToString();
 		}
-
-		private DataTable GetExcelData(string excelFilePath)
+		private DataTable GetExcelData(string excelFilePath, int skipRows = 0)
 		{
 			DataSet ds = new DataSet();
 
@@ -62,8 +83,8 @@ namespace ExcelDataLoader
 					{
 						ConfigureDataTable = _ => new ExcelDataTableConfiguration()
 						{
-							UseHeaderRow = true,
-							FilterRow = rowReader => rowReader.Depth > 2
+							UseHeaderRow = false,
+							FilterRow = rowReader => rowReader.Depth >= skipRows
 						}
 					});
 				}
@@ -78,51 +99,145 @@ namespace ExcelDataLoader
 
 			return ds.Tables[0];
 		}
+		private DataTable GetExcelPreview(string excelFilePath, int columnCount, int rowCount, int skipRows = 0)
+		{
+			DataTable dt = new DataTable();
 
-		private void BulkInsert(DataTable dataTable, string tableName)
+			for (int i = 0; i < columnCount; i++)
+				dt.Columns.Add();
+
+			using (var stream = File.Open(_excelPath, FileMode.Open, FileAccess.Read))
+			{
+				using (var reader = ExcelReaderFactory.CreateReader(stream))
+				{
+					while(skipRows > 0)
+					{
+						reader.Read();
+						skipRows--;
+					}
+
+					for (int i = 0; i < rowCount; i++)
+					{
+						bool end = reader.Read();
+						if (!end)
+							break;
+						DataRow row = dt.NewRow();
+						for (int j = 0; j < columnCount; j++)
+						{
+							object data = reader.GetValue(j);
+							row[j] = data?.ToString();
+						}
+						dt.Rows.Add(row);
+					}
+				}
+			}
+
+			return dt;
+		}
+		private void BulkInsert(DataTable dataTable, string tableName, MySqlConnection con, bool clearTable = true)
 		{
 			StringBuilder comString = new StringBuilder($"INSERT INTO {tableName} VALUES ");
 
-			using (var con = new MySqlConnection(CON_STRING))
+			con.Open();
+			using (var tran = con.BeginTransaction(IsolationLevel.Serializable))
 			{
-				con.Open();
-				using (var tran = con.BeginTransaction(IsolationLevel.Serializable))
+				if (clearTable)
 				{
 					using (var com = new MySqlCommand($"TRUNCATE TABLE {tableName};", con))
 					{
 						com.CommandType = CommandType.Text;
 						com.ExecuteNonQuery();
 					}
+				}
 
-					int currentBatchSize = 0;
-					for (int i = 0; i < dataTable.Rows.Count; i++)
+				int currentBatchSize = 0;
+				for (int i = 0; i < dataTable.Rows.Count; i++)
+				{
+					DataRow row = dataTable.Rows[i];
+
+					currentBatchSize++;
+
+					int id = Convert.ToInt32(row[_mapping["id"]]);
+					string ogrn = row[_mapping["ogrn"]].ToString();
+					string inn = row[_mapping["inn"]].ToString();
+					string name = MySqlHelper.EscapeString(row[_mapping["name"]].ToString());
+
+					comString.Append($"({id},'{ogrn}','{inn}','{name}')");
+
+					if (currentBatchSize == BATCH_SIZE || i == dataTable.Rows.Count - 1)
 					{
-						DataRow row = dataTable.Rows[i];
+						comString.Append(';');
 
-						currentBatchSize++;
-						comString.Append($"({Convert.ToInt32(row[0])},'{row[1]}','{row[2]}','{MySqlHelper.EscapeString(row[3].ToString())}')");
-
-						if (currentBatchSize == BATCH_SIZE || i == dataTable.Rows.Count - 1)
+						using (var com = new MySqlCommand(comString.ToString(), con))
 						{
-							comString.Append(';');
-
-							using (var com = new MySqlCommand(comString.ToString(), con))
-							{
-								com.CommandType = CommandType.Text;
-								com.Transaction = tran;
-								com.ExecuteNonQuery();
-							}
-
-							currentBatchSize = 0;
-							comString = new StringBuilder($"INSERT INTO {tableName} VALUES ");
+							com.CommandType = CommandType.Text;
+							com.Transaction = tran;
+							com.ExecuteNonQuery();
 						}
-						else
-							comString.Append(',');
-					}
 
-					tran.Commit();
+						currentBatchSize = 0;
+						comString = new StringBuilder($"INSERT INTO {tableName} VALUES ");
+					}
+					else
+						comString.Append(',');
+				}
+
+				tran.Commit();
+			}
+			con.Close();
+		}
+		private int GetRowCount(string tableName, MySqlConnection con)
+		{
+			int rows = 0;
+			con.Open();
+			using (MySqlCommand com = new MySqlCommand($"SELECT COUNT(1) FROM {tableName};", con))
+			{
+				com.CommandType = CommandType.Text;
+				rows = Convert.ToInt32(com.ExecuteScalar());
+			}
+			con.Close();
+			return rows;
+		}
+		private void MappingChanged(object sender, EventArgs e)
+		{
+			ComboBox changedBox = (ComboBox)sender;
+
+			_mapping[changedBox.Text] = Convert.ToInt32(changedBox.Tag);
+
+			List<int> possibleValues = Enumerable.Range(0, _mappingBoxes.Count).ToList();
+			foreach (var box in _mappingBoxes)
+				possibleValues.Remove(box.SelectedIndex);
+
+			if (possibleValues.Count == 0)
+				return;
+
+			foreach (var box in _mappingBoxes)
+			{
+				if (box != changedBox && box.SelectedIndex == changedBox.SelectedIndex)
+				{
+					box.SelectedIndex = possibleValues[0];
+					break;
 				}
 			}
+		}
+		private void skip_rows_numeric_ValueChanged(object sender, EventArgs e)
+		{
+			var numeric = (NumericUpDown)sender;
+
+			if (numeric.Value < 0)
+			{
+				numeric.Value = 0;
+				return;
+			}
+
+			if ((numeric.Value % 1) != 0)
+			{
+				numeric.Value = (int)numeric.Value;
+				return;
+			}
+
+			if (_excelPath != "")
+				excel_preview.DataSource = GetExcelPreview(_excelPath, 4, 4, (int)skip_rows_numeric.Value);
 		}
 	}
 }
